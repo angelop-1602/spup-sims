5e733b097af3643fcfd7caf93b70abee89e0b5aa
TeemoLordzz <ggnayouplease@gmail.com>
2026-06-24 16:54:30 +0800

    hrm-dashboard

---
+++ app/hrm/layout.tsx
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono, Inter } from "next/font/google"
+
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { cn } from "@/lib/utils";
+import { Providers } from "../provider";
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+
+const fontMono = Geist_Mono({
+    subsets: ["latin"],
+    variable: "--font-mono",
+})
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html
+            lang="en"
+            suppressHydrationWarning
+            className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+        >
+            <body>
+                <Providers>
+                    <TooltipProvider>{children}</TooltipProvider>
+                </Providers>
+            </body>
+        </html>
+    )
+}
