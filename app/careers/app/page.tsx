67dce02b39a11996fe7d04b4f71108e636d13e08
tinatuazon <tuazoncristina31@gmail.com>
2026-07-14 10:08:07 +0800

    move page.tsx to app

--- app/careers/app/page.tsx
+++ app/careers/app/page.tsx
@@ -14,12 +14,12 @@ import {
   ArrowRight
 } from 'lucide-react';
 import Link from 'next/link';
-import { Job, UserProfile, Application, INITIAL_JOBS } from './components/types';
-import FeaturedJobs from './components/FeaturedJobs';
-import JobDetailsModal from './components/JobDetailsModal';
-import ProcessTimeline from './components/ApplicationProcess';
-import FaqSection from './components/Faq';
-import JobBoardCTA from './components/Cta';
+import { Job, UserProfile, Application, INITIAL_JOBS } from '../components/types';
+import FeaturedJobs from '../components/FeaturedJobs';
+import JobDetailsModal from '../components/JobDetailsModal';
+import ProcessTimeline from '../components/ApplicationProcess';
+import FaqSection from '../components/Faq';
+import JobBoardCTA from '../components/Cta';
 import { Instrument_Serif, Poppins, Epilogue, Inter } from 'next/font/google';
 
 const instrumentSerif = Instrument_Serif({
